980ee91a303d522bcbda4b2ee2251b6ed2c0774d
joel <105236007+RealArtsn@users.noreply.github.com>
2024-02-15 23:58:40 -0500

    Add dropdown selection upon typing

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
 // get json data for matching
 // TODO: import from separate JSON file
-let data = JSON.parse('{"key1": "value1","key2": "value2"}');
+let data = JSON.parse('{"Dell Latitude 7740": "5TF10","thing2": "value2"}');
 
 function init() {
     // call lookup function when user types in field
-    document.querySelector('#key').addEventListener('input', () => lookup(document.querySelector("#key").value))
+    input = document.querySelector('#key');
+    input.addEventListener('input', () => lookup(document.querySelector("#key").value))
+    // reset and select field on page load
+    input.value = '';
+    input.focus();
+    // append keys as list of options
+    for (let key in data) {
+        let option = document.createElement('option');
+        option.value = key;
+        document.querySelector('#keyslist').appendChild(option);
+    }
+    
 }
 
 function lookup(key) {
     console.log('LOOKING IT UP!' + ' Key: ' + key);
     // look up the value associated with the typed key
     // TODO: sanitize input data (make case insensitive)
-    // TODO: add dropdown to select model
     document.querySelector('#ui').textContent = data[key];
 }
 
